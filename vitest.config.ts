66de3ea3ab7a36ce82d366a57c21635f8a0ef57b
cris <christoph@takacs.at>
2026-05-22 08:12:54 +0200

    added vi-test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: ['tests/**', 'node_modules/**'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html']
+    }
+  }
+});
